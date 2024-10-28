065d1bd586cd72acb9ad549393c3267c63fc502a
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-28 19:44:45 +0500

    add swiper for services

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -22,5 +22,26 @@ const slider = () => {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  const swiperServices = new Swiper('.swiper-services', {
+    modules: [Navigation],
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 10,
+    breakpoints: {
+      365: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 };
 export default slider;
