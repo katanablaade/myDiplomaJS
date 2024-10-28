324b0744fc1193bc48442034f7d08229fb68d94f
Maksim Shubny <shubny.maksim@yandex.ru>
2024-10-28 23:36:36 +0500

    add timer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import modal from './modules/modal';
 import slider from './modules/slider';
+import timer from './modules/timer';
 
 modal();
 slider();
+timer('3 november 2024');
---
+++ src/modules/timer.js
@@ -0,0 +1,47 @@
+const timer = (deadline) => {
+  const timerDay = document.querySelector('.count_1 span');
+  const timerHours = document.querySelector('.count_2 span');
+  const timerMinutes = document.querySelector('.count_3 span');
+  const timerSeconds = document.querySelector('.count_4 span');
+  let intervalId;
+
+  const getTimeRemaining = () => {
+    const dateStop = new Date(deadline).getTime();
+    const dateNow = new Date().getTime();
+    const timeRemaining = (dateStop - dateNow) / 1000;
+    let days = Math.floor(timeRemaining / 60 / 60 / 24);
+    let hours = Math.floor((timeRemaining / 60 / 60) % 24)
+      .toString()
+      .padStart(2, '0');
+    let minutes = Math.floor((timeRemaining / 60) % 60)
+      .toString()
+      .padStart(2, '0');
+    let seconds = Math.floor(timeRemaining % 60)
+      .toString()
+      .padStart(2, '0');
+
+    return { timeRemaining, days, hours, minutes, seconds };
+  };
+
+  const updateClock = () => {
+    const getTime = getTimeRemaining();
+
+    if (getTime.timeRemaining < 0) {
+      timerDay.textContent = '00';
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+      clearInterval(intervalId);
+    } else {
+      timerDay.textContent = getTime.days;
+      timerHours.textContent = getTime.hours;
+      timerMinutes.textContent = getTime.minutes;
+      timerSeconds.textContent = getTime.seconds;
+    }
+  };
+
+  intervalId = setInterval(updateClock, 1000);
+  updateClock();
+};
+
+export default timer;
